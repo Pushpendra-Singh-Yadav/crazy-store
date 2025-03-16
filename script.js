6431a18c7c3778d23030cdab3bcd40808a7378b9
Pushpendra Singh Yadav <psychd2005@gmail.com>
2025-03-16 22:57:04 +0530

    7

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
+// Elements
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
 
-navToggle.addEventListener("click", () => {
-  const isExpanded = navToggle.getAttribute("aria-expanded") === "true";
-  navToggle.setAttribute("aria-expanded", !isExpanded);
-  navMenu.classList.toggle("show");
+// Toggle Menu
+navToggle.addEventListener('click', () => {
+  const isExpanded = navToggle.getAttribute('aria-expanded') === 'true';
+  navToggle.setAttribute('aria-expanded', !isExpanded);
+  navMenu.classList.toggle('active');
+});
+
+// Close menu on clicking outside
+document.addEventListener('click', (e) => {
+  if (!navMenu.contains(e.target) && !navToggle.contains(e.target)) {
+    navMenu.classList.remove('active');
+    navToggle.setAttribute('aria-expanded', 'false');
+  }
 });
